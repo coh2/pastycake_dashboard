2f465d2f2d33cacffd34ce391daa84eb55e2cc74
Clemens-O. Hoppe <coh@co-hoppe.net>
2012-02-23 22:54:26 +0100

    relative-path config file support fixed, removed the config dump from the runner, fixed a logic bug for the localhost check

--- flask/run.py
+++ flask/run.py
@@ -4,6 +4,7 @@ from __future__ import division
 
 import math
 import os
+import os.path
 import sys
 
 from pastycake_dashboard.pastydash import app
@@ -14,21 +15,18 @@ def _setup_security(appobj):
     setup required security features.
     '''
     # adjusted for imperfect entropy based on tests under linux
-    appobj.secret_key = os.urandom(int(math.ceil(256 / 7.991)))
+    appobj.secret_key = os.urandom(int(math.ceil(256 / 7.995)))
 
     host = app.config.get('HOST', '127.0.0.1')
 
-    if (not host.startswith('127.') or
+    if (not host.startswith('127.') and
         host not in ('::1', '0:0:0:0:0:0:0:1')) and appobj.debug:
         raise RuntimeError('public facing + debug enabled = you WILL get \
                             owned')
 
-
 if len(sys.argv) > 1:
-    app.config.from_pyfile(sys.argv[1])
+    conffile = os.path.isabs(sys.argv[1]) or os.path.abspath(sys.argv[1])
+    app.config.from_pyfile(conffile)
 
-for k, v in app.config.items():
-    print k, v
 _setup_security(app)
-
 app.run(host=app.config.get('HOST', '127.0.0.1'))
