010b70a1e066c3766809ca99e69600e08534295e
Clemens-O. Hoppe <coh@co-hoppe.net>
2012-02-25 17:30:13 +0100

    done with the ajax dom rendering

--- flask/pastycake_dashboard/pastydash.py
+++ flask/pastycake_dashboard/pastydash.py
@@ -1,11 +1,36 @@
 '''main component of the dashboard'''
-from flask import render_template
+from flask import render_template, jsonify, request, abort
 
 from . import app
 
 import crypto
+import datetime
+
+
+backend = None
+
+
+def _load_backend():
+    #TODO
+    return True
 
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+
+@app.route('/get_latest')
+def latest():
+    if not backend:
+        _load_backend() or abort(503)
+
+    # at most 20 seconds ago with an id > maxid
+    timestamp = datetime.datetime.utcnow() - datetime.timedelta(seconds=20)
+    maxid = request.args.get('id', -1, type=int)
+    (maxid, matches) = backend.get_matches(since=timestamp, maxid=maxid)
+
+    dom = render_template('ajax_latest.html', matches=matches)
+    data = {'id': maxid, 'dom': dom}
+
+    return jsonify(data)
--- flask/pastycake_dashboard/static/js/script.js
+++ flask/pastycake_dashboard/static/js/script.js
@@ -2,13 +2,23 @@
 
 */
 
-(function poll(reqobj, result){
-    if (result === 'error' || result === 'abort') {
-        return;
-    }
-    $.ajax({ url: "http://127.0.0.1:5000/get_latest", success: function(data){
-        $("#matches").prepend(data);
-    }, dataType: "json", complete: poll, timeout: 30000 });
-})(null, '');
+var pollTimer, maxId=-1;
 
+function poll_once(){
+    $.ajax({ url: "/get_latest", data: {id: maxId}, dataType: "json", timeout: 12000, success: function(data) {
+        if(data.id < maxId) {
+            return;
+        }
+        else {
+            maxId = data.id;
+        }
+        $('#matches').prepend(data.dom);
+    }, error: function() {
+        alert('connection failed. stopping polling.');
+        clearInterval(pollTimer);
+    }
+    });
+};
+
+pollTimer = setInterval(poll_once, 15000);
 
