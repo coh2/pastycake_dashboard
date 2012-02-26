59399e5b76934d63d79fe74d776ae86e73feee47
Clemens-O. Hoppe <coh@co-hoppe.net>
2012-02-26 19:53:59 +0100

    first working version, limited to sqlite backend

--- flask/pastycake_dashboard/pastydash.py
+++ flask/pastycake_dashboard/pastydash.py
@@ -1,18 +1,41 @@
 '''main component of the dashboard'''
-from flask import render_template, jsonify, request, abort
+from flask import render_template, jsonify, request
 
 from . import app
 
-import crypto
 import datetime
+import sqlite3
 
 
-backend = None
+def _sqlite_conn(with_cursor=True):
+    conn = sqlite3.connect(app.config.get('SQLITE_DB', 'urls.db'))
+    if with_cursor:
+        curs = conn.cursor()
+        return (conn, curs)
+    else:
+        return conn
 
 
-def _load_backend():
-    #TODO
-    return True
+def _get_sqlite_matches(sqlitecon, since=None, startwith=None):
+    curs = sqlitecon.cursor()
+    newmax = int(curs.execute('SELECT max(id) FROM \
+                                url_matches').fetchone()[0])
+    curs.execute('''
+        SELECT urls.url, match_expression, matched
+        FROM url_matches JOIN urls JOIN matchers
+        WHERE url_matches.url=urls.id AND url_matches.matcher=matchers.id
+            AND viewed >= ? AND url_matches.id > ? and url_matches.id <= ?
+            ORDER BY viewed DESC
+    ''', (since or datetime.datetime(1, 1, 1),
+          startwith or 0,
+          newmax
+         )
+    )
+
+    res = []
+    for _ in curs.fetchall():
+        res.append(dict(zip(('source_link', 'match_expression', 'match'), _)))
+    return (newmax, res)
 
 
 @app.route('/')
@@ -22,15 +45,16 @@ def main():
 
 @app.route('/get_latest')
 def latest():
-    if not backend:
-        _load_backend() or abort(503)
-
     # at most 20 seconds ago with an id > maxid
     timestamp = datetime.datetime.utcnow() - datetime.timedelta(seconds=20)
     maxid = request.args.get('id', -1, type=int)
-    (maxid, matches) = backend.get_matches(since=timestamp, maxid=maxid)
+
+    conn = _sqlite_conn(False)
+    (maxid, matches) = _get_sqlite_matches(conn, since=timestamp, startwith=maxid)
+    conn.close()
 
     dom = render_template('ajax_latest.html', matches=matches)
     data = {'id': maxid, 'dom': dom}
 
-    return jsonify(data)
+    res = jsonify(data)
+    return res
